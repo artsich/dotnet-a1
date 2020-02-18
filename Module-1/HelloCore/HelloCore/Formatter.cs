159ac4bfb297db6eda59afc126b45343c7649eb0
Artsiom Dubinevich <Artsiom_Dubinevich@epam.com>
2020-02-18 14:09:47 +0300

    Complete first task from module

---
+++ Module-1/HelloCore/HelloCore/Formatter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloCore
+{
+    public static class Formatter
+    {
+        public static string FormatToDateHello(string name)
+        {
+            return $"{DateTime.Now} Hello, {name}!";
+        }
+    }
+}
---
+++ Module-1/HelloCore/HelloCore2.0/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace HelloCore2._0
+{
+    public class Class1
+    {
+    }
+}
