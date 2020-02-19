54f43e9f4a52ca32ae5d560142d8ea84a0f2c592
Artsiom Dubinevich <Artsiom_Dubinevich@epam.com>
2020-02-19 11:22:45 +0300

    Done:android app

---
+++ Module-1/HelloCore/MobileHello/MobileHello/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MobileHello
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            var entry = (Entry)sender;
+            HelloLabel.Text = HelloCore.Formatter.FormatToDateHello(entry.Text);
+        }
+    }
+}
