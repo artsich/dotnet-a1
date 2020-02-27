dd3741897519a4a04206d9a79234795314f786e7
Artsiom Dubinevich <Artsiom_Dubinevich@epam.com>
2020-02-27 17:26:34 +0300

    Configuration test-app

---
+++ Module-2/FileSystemWatcher/FileSystemWatcher/FileSystemWatcher/Program.cs
@@ -0,0 +1,21 @@
+﻿using FileSystemWatcher.Settings;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace FileSystemWatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var appSettings = config.GetSection("AppSettings").Get<Setting>();
+            Console.WriteLine($"Localization: {appSettings.Localization}");
+        }
+    }
+}
---
+++ Module-2/FileSystemWatcher/FileSystemWatcher/FileSystemWatcher/Settings/Setting.cs
@@ -0,0 +1,12 @@
+﻿
+using Newtonsoft.Json;
+
+namespace FileSystemWatcher.Settings
+{
+    [JsonObject("AppSettings")]
+    public class Setting
+    {
+        [JsonProperty("Localization")]
+        public string Localization { get; set; }
+    }
+}
