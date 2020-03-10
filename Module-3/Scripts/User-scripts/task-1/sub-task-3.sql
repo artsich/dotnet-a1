c53d480f9a506fd2e5cf00852e2659952cc341d0
Artsiom Dubinevich <Artsiom_Dubinevich@epam.com>
2020-03-10 18:10:08 +0300

    add db, and few queries.

---
+++ Module-3/Scripts/User-scripts/task-1/sub-task-3.sql
@@ -0,0 +1,12 @@
+﻿select * from Northwind.[Order Details];
+
+--#1
+--select distinct OrderId from Northwind.[Order Details] as Details
+--where Details.Quantity between 3 and 10
+
+--#2 have not ended
+--------select CustomerId, Country from Northwind.Customers
+--------where Country between 'b' and 'g'
+--------order by Country;
+
+--#
